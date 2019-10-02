36bdcd859d6f21b48e2feef091d96fef41c71166
mitchcutts <mcutts@mymail.mines.edu>
2019-10-02 14:41:05 -0600

    Reversed prior commit's actions (restored eye separation to original value). Also changed meow string to hail the mighty feline

--- DrawPlay/src/Cat.java
+++ DrawPlay/src/Cat.java
@@ -9,7 +9,7 @@ public class Cat {
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 50;
 	private static final int EYE_WIDTH = 10;
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Hail the Wicked Feline", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
